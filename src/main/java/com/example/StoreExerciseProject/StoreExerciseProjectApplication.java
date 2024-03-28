fb930624fa642132cb35baaa829db3a724dbc3c6
afrentescu <adela.frentescu@gmail.com>
2024-03-28 11:57:41 +0200

    Initial commit

---
+++ src/main/java/com/example/StoreExerciseProject/StoreExerciseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.StoreExerciseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreExerciseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreExerciseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StoreExerciseProject/StoreExerciseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreExerciseProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreExerciseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
